fc8c628833f556072c1242d6afc0552fe859f562
anapedra <anapedra.mil@gmail.com>
2025-08-26 23:51:56 -0300

    Project Sudoku Backend Created

---
+++ backend/saduko/src/main/java/com/anasantana/saduko/SadukoApplication.java
@@ -0,0 +1,13 @@
+package com.anasantana.saduko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadukoApplication.class, args);
+	}
+
+}
---
+++ backend/saduko/src/test/java/com/anasantana/saduko/SadukoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasantana.saduko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadukoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
